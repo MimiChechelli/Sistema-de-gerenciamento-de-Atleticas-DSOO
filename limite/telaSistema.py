f7b91351d3c80ef4f724ded2f3b72251228e12dd
Michelli Patricia Luvison <126841298+MimiChechelli@users.noreply.github.com>
2024-06-10 20:04:17 -0300

    Update telaSistema.py

--- limite/telaSistema.py
+++ limite/telaSistema.py
@@ -1,15 +1,42 @@
-
+import PySimpleGUI as sg
 
 class TelaSistema:
+    def __init__(self) -> None:
+        self.__window = None
+        self.init_components()
+
+
+    def init_components(self):
+            layout = [
+            [sg.Text('Incluir novo cliente')],
+            [sg.Text('Nome:', size=(15,1)), sg.InputText('nome', key='nome')],
+            [sg.Text('CPF:', size=(15,1)), sg.InputText('cpf', key='cpf')],
+            [sg.Submit(), sg.Cancel()]
+            ]
+            window = sg.Window('Cadastro de clientes').Layout(layout)
+            button, values = window.read()
+
+    
+
     def tela_opcoes(self):
-        print("-------- Sistema de Campeonatos ---------")
-        print("Escolha sua opcao")
-        print("1 - Alunos")
-        print("2 - Árbitros")
-        print("3 - Atleticas")
-        print("4 - Partidas")
-        print("5 - Campeonato")
-        print("0 - Finalizar sistema")
-        opcao = int(input("Escolha a opcao: "))
-        print("\n")
-        return opcao
+        self.init_components()
+        button, values = self.__window.read()
+        self.close()
+        return button
+
+    def close(self):
+         self.__window.Close()
+    
+    def init_components(self):
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+             [sg.Text('Bem vindo')],
+             [sg.Text('escolha a opção:')],
+             [sg.Button('Alunos', key=1)],
+             [sg.Button('Arbitros', key=2)],
+             [sg.Button('Atleticas', key=3)],
+             [sg.Button('Partidas', key=4)],
+             [sg.Button('Campeonato', key=5)],
+             [sg.Button('Encerrar', key=0)],
+        ]
+        self.__window = sg.Window('Sistema de Atleticas').Layout(layout)

4000e70efb32f3525e58d4b121c68d35b9b163a0
Ferrgabe <ferrgabe@outlook.com>
2024-06-28 15:38:49 -0300

    Alteraçoes DAO

--- dao/alunoDao.py
+++ dao/alunoDao.py
@@ -1,10 +1,18 @@
 from abstractDao import DAO
 from entidade.aluno import Aluno
+
 class AlunoDAO(DAO):
     def __init__(self):
-        super().__init__('clientes.pkl')
+        super().__init__('alunos.pkl')
     
     def add(self, aluno: Aluno):
         if (isinstance(aluno.cpf, int)) and (aluno is not None) and isinstance(aluno, Aluno):
             super().add(aluno.codigo, aluno)
-            #parei no slide 21
+    
+    def get(self, key: int):
+        if isinstance(key ,int):
+            return super().get(key)
+        
+    def remove(self, key:int):
+        if isinstance(key, int):
+            return super().remove(key)
